5f5af81f16d714570d28a25ec6e936d1aa57931f
Mister Zafarov <110981143+mrzafarov@users.noreply.github.com>
2022-10-27 23:05:31 +0500

    Update and rename function.py to son-top-consol.py
    Signed-off-by: Mister Zafarov <110981143+mrzafarov@users.noreply.github.com>

--- son-top-consol.py
+++ son-top-consol.py
@@ -14,7 +14,7 @@ def sontop(x=10):
             print("Xato, men o'ylagan son bundan kichikroq. Qaytadan urinib ko'ring:")
         else:
              break
-    print(f"Tabriklayman TOPDINGIZ! {taxminlar} ta taxmin bilan topdingiz.\n")
+    print(f"TABRIKLAYMAN! {taxminlar} ta taxmin bilan topdingiz!\n")
     return taxminlar
 
 
@@ -30,7 +30,7 @@ def sontop_pc(x=10):
             taxmin = random.randint(quyi,yuqori)
         else:
             taxmin = quyi
-        javob = input(f"Taxminimcha siz {taxmin} sonini o'yladingiz. Agar to'g'ri bo'lsa (T),"
+        javob = input(f"Taxminimcha siz {taxmin} sonini o'yladingiz. Agar to'g'ri bo'lsa (t),"
                       f" siz o'ylagan son bundan kattaroq bo'lsa (+), kichikroq bo'lsa (-) ni kiriting: ")
         if javob == "-":
             yuqori = taxmin - 1
@@ -38,5 +38,20 @@ def sontop_pc(x=10):
             quyi = taxmin + 1
         else:
             break
-    print(f"\nTOPDIM! {taxminlar} ta taxmin bilan topdim.")
+    print(f"\nURRAAA! {taxminlar} ta taxmin bilan topdim!")
     return taxminlar
+    
+def play(x=10):
+    yana = True
+    while yana:
+        user = sontop(x)
+        pc = sontop_pc(x)
+        if user < pc:
+            print(f"\nSiz YUTDINGIZ! Siz {user} ta taxmin bilan, men esa {pc} ta taxmin bilan topdim!")
+        elif user > pc:
+            print(f"\nMen YUTDIM! Men {pc} ta taxmin bilan, siz esa {user} ta taxmin bilan topdingiz!")
+        else:
+            print(f"\nDURRANG! Ikkimiz ham {user} ta taxmin bilan topdik!")
+        yana = int(input("Yana o'ynaymizmi? Ha(1)/Yo'q(0): "))
+             
+play()
